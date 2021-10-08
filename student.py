2376eeb53138fc604b2e073aa214681e8a0817e7
Rishika D S <89212073+rishikads@users.noreply.github.com>
2021-10-09 00:49:58 +0530

    WIP

---
+++ student.py
@@ -0,0 +1,59 @@
+from typing import ItemsView, ValuesView
+from flask import Flask , request , url_for
+import json, os, requests
+app = Flask(__name__)
+@app.route('/' , methods=['POST', 'GET', 'PUT', 'DELETE'])
+def a_operations():
+    if(request.method=='GET'):
+       a=open("student.json", 'r')
+       a= a.read()
+       #a.close()
+       #return str((type(b)))
+       return a 
+    if(request.method=='POST'):
+        data= request.get_json()
+        print(data)
+        print(type(data))
+        path = os.path.join(os.getcwd(),'student.json')
+        a= open(path, 'r+')
+        a= a.read()
+        a= json.loads(a)
+        b= a["students"]
+        b[str(data['id'])]= data['name']
+        print(a)
+        return "update successful"
+    if(request.method=='PUT'):
+        data= request.get_json()
+        print(data)
+        print(type(data))
+        path = os.path.join(os.getcwd(),'student.json')
+        a= open(path, 'r+')
+        a= a.read()
+        a= json.loads(a)
+        for i in a["students"] :
+            if (i==data['id']):
+                a["students"][i]= data['name']
+                a.append(data)
+                return "update successful"
+            else:
+                return "update unsuccessful"
+    if(request.method=='DELETE'):
+        data= request.get_json()
+        print(data)
+        print(type(data))
+        path = os.path.join(os.getcwd(),'student.json')
+        a= open(path, 'r+')
+        a= a.read()
+        a= json.loads(a)
+        for i in a["students"] :
+            if (i==data['id']):
+                a["students"][i]= data['name']
+                a=open("student.json", 'w')
+                a.write(None)
+                return "delete successful"
+            else:
+                return "delete unsuccessful"
+if __name__ == '__main__' :
+  app.run(debug=True, port=5000)
+
+
